ff98d54832f85b8c4b69e45252948679de10fa16
XenomorphingTV <sconovali@hotmail.com>
2021-10-31 18:40:06 +0100

    Changed program to use pointers for optimization, removed cout for optimization (cuts time for 300000 by average of 12 secs)

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -13,16 +13,16 @@ int main()
 	srand(std::time(NULL));
 
 	// Time of evolution, will change this to input later
-	int time{ 300 };
+	int time{ 300000 };
 
 	// Create our creature, will change to input later
 	Creature model1(1, 0, 0.2);
 
 	// Our starting population
-	int population{ 0 };
+	int population = 0;
 
 	// Run our basic simulation
-	basicSimulation(model1, population, time);
+	basicSimulation(model1, &population, time);
 
 	// Print the recorded time of program
 	printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
