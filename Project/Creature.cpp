c27ce3e058b241c7e66eacf2dadf903af62a17d8
XenomorphingTV <sconovali@hotmail.com>
2021-10-31 12:30:29 +0100

    Added comments, changed output file type

--- Project/Creature.cpp
+++ Project/Creature.cpp
@@ -1,16 +1,17 @@
 #include "Creature.h"
 #include <ctime>
 #include <random>
-//Constructor
 
 std::mt19937 rng(std::time(NULL));
 
+// Constructor
 Creature::Creature(int sbr, double rc, double dc) {
 	spontaneousBirthRate = sbr;
 	replicationChance = rc;
 	deathChance = dc;
 }
-	
+
+// Return private data fields
 int Creature::getSpontaneousBirthRate() {
 	return spontaneousBirthRate;
 }
@@ -23,6 +24,7 @@ double Creature::getDeathChance() {
 	return deathChance;
 }
 
+// Random result based on percentage chance
 bool Creature::randomPercentCheck(double percentage) {
 	static const std::uniform_real_distribution<double> dist(0, 1);
 	bool passed{ dist(rng) < percentage };
--- Project/Simulation.cpp
+++ Project/Simulation.cpp
@@ -6,32 +6,42 @@
 #include <stdlib.h>
 
 void basicSimulation(Creature model, int population, int time) {
-	std::cout << "Time: 0 | Population: " << population << "\n";
-	std::remove("GraphPlots.dat");
+	// Remove previously created file, becuase we always append to it
+	std::remove("GraphPlots.csv");
 
+	// Output and save population at the time of 0
+	std::cout << "Time: 0 | Population: " << population << "\n";	
 	saveCoords(0, population);
+
+	// Loop to go through the time from zero until the specified number
 	for (int currentTime = 1; currentTime <= time; currentTime++) {
+
+		// Using async to make our program faster by multithreading
 		std::future<int> deathChanceCalcAsync = std::async(std::launch::async, deathChanceCalulator, population, model);
+
+		// Do all the maths for population, add the spontaneous birth rate before doing any other calculations
 		population = population + model.getSpontaneousBirthRate();
-		int deaths = 0;
+		int deaths = deathChanceCalcAsync.get();
 		int births = replicationChanceCalulator(population, model);
-		try {
-			deaths = deathChanceCalcAsync.get();
-		}
-		catch (std::future_error& e) {
-			std::cout << "future_error caught: " << e.what() << '\n';
-		}
 		population = population + (births - deaths);
+
+		// Check if the population ever drops below zero, if it does then make population zero
 		population = (population < 0) ? 0 : (population);
+
+		// Output the data to screen, change the modulus to change how often you get results outputted
 		if (currentTime % 100 == 0) {
 			std::cout << "Time: " << currentTime << " | " << "Population: " << population << "\n";
 		}
+
+		// Save the data to a .cvs file, will later be edited to plot graphs using gnu-plot
 		saveCoords(currentTime, population);
 	}
 }
 
 int deathChanceCalulator(int population, Creature model) {
 	int tempPop{ 0 };
+
+	// Check how many creatures die in the population, store them in a temp value to take away from the population later
 	for (int i = 0; i <= population; i++) {
 		if (model.randomPercentCheck(model.getDeathChance())) {
 			tempPop++;
@@ -42,6 +52,8 @@ int deathChanceCalulator(int population, Creature model) {
 
 int replicationChanceCalulator(int population, Creature model) {
 	int tempPop{ 0 };
+
+	// Same as deathChanceCalculator, but we add the temp value to population later
 	for (int i = 0; i <= population; i++) {
 		if (model.randomPercentCheck(model.getReplicationChance())) {
 			tempPop++;
@@ -52,6 +64,8 @@ int replicationChanceCalulator(int population, Creature model) {
 
 void saveCoords(int time, int population) {
 	std::ofstream MyFile;
-	MyFile.open("GraphPlots.dat", std::ios_base::app);
-	MyFile << time << "\t" << population << "\n";
+	MyFile.open("GraphPlots.csv", std::ios_base::app);
+
+	// The required template for cvs files
+	MyFile << time << "," << population << "\n";
 }
